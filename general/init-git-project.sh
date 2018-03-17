2c0054a05e3a26437a35a2ca8fd9ab0b0b1f00b9
Henrique Rangel <hrangel@hrangel.com.br>
2018-03-17 19:25:41 -0300

    Corrigido script de gerar git

--- general/init-git-project.sh
+++ general/init-git-project.sh
@@ -2,10 +2,11 @@
 project_folder=${PWD##*/}
 echo "Setup GIT for $project_folder"
 
+cd $project_folder
+
 git init
-git checkout -b development
 git checkout -b staging
 git checkout -b live
-git checkout development
+git checkout -b development
 
 echo "GIT initialized & branches (development, staging, live) on $project_folder"
